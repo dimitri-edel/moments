0ee66fa9a2594638bd234f0fd2764224e8008647
dimitri-edel <>
2023-07-27 12:25:23 +0000

    Add Create Post page, without handling the post request yet. pages/posts/PostCreateForm.js, hooks/useClickOutsideToggle.js, components/Asset.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,19 @@ import React from "react";
 import logo from "../assets/logo192.png";
 import styles from "./styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-// import { useCurrentUser } from "../contexts/CurrentUserContext";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 import {
-    useCurrentUser,
-    useSetCurrentUser,
-  } from "../contexts/CurrentUserContext";
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -81,7 +82,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -89,7 +90,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
---
+++ src/pages/posts/PostCreateForm.js
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+
+import Upload from "../../assets/upload.png";
+
+import styles from "../../components/styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../components/styles/Button.module.css";
+import Asset from "../../components/Asset";
+
+function PostCreateForm() {
+  const [errors, setErrors] = useState({});
+  const [postData, setPostData] = useState({
+    title: "",
+    Content: "",
+    image: "",
+  });
+
+  const { title, content, image } = postData;
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.taget.name]: event.taget.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+  const textFields = (
+    <div className="text-center">
+      {/* Add your form fields here */}
+
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Blue}`}
+        onClick={() => {}}
+      >
+        cancel
+      </Button>
+      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+        create
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+              <Form.Group>
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="title"
+                  value={title}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+
+              <Form.Group>
+                 {/* NOTE : the Lables below have the htmlFor attribute that is assigned
+                    to "image-upload". Meaning, if those components get clicked on
+                    the onChange - event handler of the Form.File compoenent down below 
+                    will be executed. Because the Form.File has the id="image-upload".
+                  */}
+                {image ? (
+                  <>
+                    <figure>
+                      <Image className={appStyles.Image} src={image} rounded />
+                    </figure>
+                    <div>
+                      <Form.Label
+                        className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                        htmlFor="image-upload"
+                      >
+                        Change the image
+                      </Form.Label>
+                    </div>
+                  </>
+                ) : (
+                  <Form.Label
+                    className="d-flex justify-content-center"
+                    htmlFor="image-upload"
+                  >
+                    <Asset
+                      src={Upload}
+                      message="Click or tap to uplaod an image"
+                    />
+                  </Form.Label>
+                )}
+                {/* NOTE : the Lables above have the htmlFor attribute that is assigned
+                    to the component below. Meaning, if those components get clicked on
+                    the onChange - event handler of this compoenent will be executed.
+                  */}
+                <Form.File
+                  id="image-upload"
+                  accept="image/*"
+                  onChange={handleChangeImage}
+                />
+
+                <Form.Label>Content</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  name="content"
+                  rows={6}
+                  value={content}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Form.Group>
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default PostCreateForm;
