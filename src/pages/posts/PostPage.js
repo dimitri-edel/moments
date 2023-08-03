f6b219e2f03f231d5407b3a412979e4b5d19db65
dimitri-edel <>
2023-08-03 09:59:01 +0000

    Display filtered posts with search bar and filters: liked, follwoed profile

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,6 +7,8 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefault";
+import Post from "./Post";
+
 
 function PostPage() {
   // Retrieve the id from URL-GET-Request, which was previously set in App.js
@@ -34,7 +36,7 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
+        <p><Post {...post.results[0]} setPost={setPost}/></p>
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
